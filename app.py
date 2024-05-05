ad570beebd0fcb3c187a469d1cc1bda62b0f2eec
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-06 04:32:58 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import tensorflow as tf
+
+@st.cache(allow_output_mutation=True)
+def load_model():
+  model=tf.keras.models.load_model('model.h5')
+  return model
+model=load_model()
+st.write("""
+# Garbage Classification"""
+)
+file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
+
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(128,128)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
+    return prediction
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
+    class_names=['cardboard',
+                 'glass',
+                 'metal',
+                 'paper',
+                 'plastic',
+                 'trash']
+    string="OUTPUT : "+class_names[np.argmax(prediction)]
+    st.success(string)
