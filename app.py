1ec749c9479b253af2b9e36d17fd70307735e814
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-06 18:20:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import tensorflow as tf
-import keras.utils as ku
+from tensorflow.keras.preprocessing.image import img_to_array
 
 @st.cache(allow_output_mutation=True)
 def load_model():
