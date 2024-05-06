0dbd7b79ea9ea03dcbe20446e378f0e1e0e90564
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-06 17:35:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ else:
                  'paper',
                  'plastic',
                  'trash']
-    string="Probability : "+np.max(prediction[0]
+    string="Probability : "+np.max(prediction[0])
     string="OUTPUT : "+class_names[np.argmax(prediction)]
     st.success(string)
