e64d51cb4eb1bbce9b310017d1c8cdf3c9e07f19
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-06 18:50:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,32 @@
 import streamlit as st
 import tensorflow as tf
-import numpy as np
-from PIL import Image, ImageOps
-from tensorflow.keras.preprocessing.image import img_to_array
-from keras.models import load_model
 
 @st.cache(allow_output_mutation=True)
-def load_fashion_model():
-    model = tf.keras.models.load_model('Garbagemodel.h5')
-    return model
+def load_model():
+  model=tf.keras.models.load_model('MultiWeatherCNN.hdf5')
+  return model
+model=load_model()
+st.write("""
+# Weather System"""
+)
+file=st.file_uploader("Choose photo from computer",type=["jpg","png"])
 
-def import_and_predict(image_data, model):
-    size = (32, 32)
-
-    # Ensure image_data is in the correct data type and range
-    image_data = (image_data / 255).astype(np.uint8)
-    # Convert the NumPy array to an Image instance
-    image = Image.fromarray(image_data)
-    # Use ImageOps.fit with the Image instance
-    
-    image = ImageOps.fit(image, size)
-    img = np.asarray(image)
-    
-    img = img[:, :, 0]
-
-    img_reshape = img[np.newaxis, ..., np.newaxis]
-    prediction = model.predict(img_reshape)
+import cv2
+from PIL import Image,ImageOps
+import numpy as np
+def import_and_predict(image_data,model):
+    size=(128,128)
+    image=ImageOps.fit(image_data,size)
+    img=np.asarray(image)
+    img_reshape=img[np.newaxis,...]
+    prediction=model.predict(img_reshape)
     return prediction
-
-def load_image():
-
-    
-    # Check if the image is grayscale, if so, add a channel dimension
-    if len(img.shape) == 2:
-        img = np.expand_dims(img, axis=-1)
-    
-    img = img / 255.0
-    img = np.reshape(img, (1, 32, 32, img.shape[-1]))
-    return img
-
-model = load_fashion_model()
-
-st.write("""# Fashion Dataset by Group 3""")
-file = st.file_uploader("Choose photo from computer", type=["jpg", "png"])
-
 if file is None:
     st.text("Please upload an image file")
 else:
-    image = Image.open(file)
-    st.image(image, use_column_width=True)
-
-    # Convert the image to a NumPy array
-    image_array = img_to_array(image)
-
-    # Normalize the image
-    image_array = image_array / 255.0
-
-    # Load the image into the model for prediction
-    prediction = import_and_predict(image_array, model)
-
+    image=Image.open(file)
+    st.image(image,use_column_width=True)
+    prediction=import_and_predict(image,model)
     class_names=['cardboard',
                  'glass',
                  'metal',
@@ -66,6 +34,7 @@ else:
                  'plastic',
                  'trash']
     result_class = np.argmax(prediction)
+    result_class1 = np.max(prediction)
     result_label = class_names[result_class]
-    string = f"Prediction: {result_label} ({prediction[0][result_class]:.2%} confidence)"
+    string = f"Prediction: {result_label} ({prediction[0][result_class1]:.2%} confidence)"
     st.success(string)
