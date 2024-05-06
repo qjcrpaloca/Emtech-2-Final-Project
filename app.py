67ff87ff7a5ec106f65d89709ca9a2c7c6b64c44
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-06 18:50:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  model=tf.keras.models.load_model('MultiWeatherCNN.hdf5')
+  model=tf.keras.models.load_model('Garbagemodel.h5')
   return model
 model=load_model()
 st.write("""
@@ -15,7 +15,7 @@ import cv2
 from PIL import Image,ImageOps
 import numpy as np
 def import_and_predict(image_data,model):
-    size=(128,128)
+    size=(32,32)
     image=ImageOps.fit(image_data,size)
     img=np.asarray(image)
     img_reshape=img[np.newaxis,...]
