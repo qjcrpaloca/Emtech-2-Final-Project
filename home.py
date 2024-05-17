9cb58fc4a7c5900a00d3f21b3e82381bcb9b5eae
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 21:19:17 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
 st.sidebar.title("Trash Classifier App")
-st.sidebar.markdown("# [Home](https://emtech-2-final-project-home.streamlit.app/)")
-st.sidebar.markdown("# [Application](https://emtech-2-final-project-application.streamlit.app/)")
-st.sidebar.markdown("# [Contributors](https://emtech-2-final-project-contributors.streamlit.app/)")
+st.page_link("home.py",label="Home")
+st.page_link("app.py",label="Application")
+st.page_link("contributors.py",label="Contributors")
+#st.sidebar.markdown("# [Home](https://emtech-2-final-project-home.streamlit.app/)")
+#st.sidebar.markdown("# [Application](https://emtech-2-final-project-application.streamlit.app/)")
+#st.sidebar.markdown("# [Contributors](https://emtech-2-final-project-contributors.streamlit.app/)")
 
 page_bg_img = '''
 <style>
