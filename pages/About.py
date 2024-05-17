6c6b8e641505356ad0ab4ab51584eacec177a39e
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 21:52:27 +0800

    Update and rename Trash Classifier.py to About.py

---
+++ pages/About.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.sidebar.title("Trash Classifier App")
+st.sidebar.markdown("# [Home](https://emtech-2-final-project-home.streamlit.app/)")
+st.sidebar.markdown("# [Application](https://emtech-2-final-project-application.streamlit.app/)")
+st.sidebar.markdown("# [Contributors](https://emtech-2-final-project-contributors.streamlit.app/)")
+
+page_bg_img = '''
+<style>
+.main {
+background-image: url("https://static.vecteezy.com/system/resources/previews/034/346/838/non_2x/different-colored-recycle-waste-bins-illustration-waste-types-segregation-recycling-vector.jpg");
+background-size: cover;
+}
+</style>
+'''
+st.markdown(page_bg_img, unsafe_allow_html=True)
