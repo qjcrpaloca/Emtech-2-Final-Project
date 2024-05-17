0bf565722e8520cbef28bb516218ce0612aaed02
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 21:04:42 +0800

    Update home.py

--- home.py
+++ home.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.markdown("# Page 2 ❄️")
-st.sidebar.markdown("# Home ❄️")
+st.sidebar.markdown("# [Home](https://emtech-2-final-project-home.streamlit.app/)")
+st.sidebar.markdown("# [Application](https://emtech-2-final-project-application.streamlit.app/)")
+st.sidebar.markdown("# [Contributors](https://emtech-2-final-project-contributors.streamlit.app/)")
