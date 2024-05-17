eebe590c4dfbdcde29b1f523ae1a991f5360460f
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 21:07:27 +0800

    Update contributors.py

--- contributors.py
+++ contributors.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.sidebar.title("Trash Classifier App")
 st.sidebar.markdown("# [Home](https://emtech-2-final-project-home.streamlit.app/)")
 st.sidebar.markdown("# [Application](https://emtech-2-final-project-application.streamlit.app/)")
 st.sidebar.markdown("# [Contributors](https://emtech-2-final-project-contributors.streamlit.app/)")
