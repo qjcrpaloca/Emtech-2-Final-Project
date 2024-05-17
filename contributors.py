276f9478053539f86c9a6eb55938bfb6f9a1c5b4
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 21:04:52 +0800

    Update contributors.py

--- contributors.py
+++ contributors.py
@@ -1,2 +1,5 @@
 import streamlit as st
 
+st.sidebar.markdown("# [Home](https://emtech-2-final-project-home.streamlit.app/)")
+st.sidebar.markdown("# [Application](https://emtech-2-final-project-application.streamlit.app/)")
+st.sidebar.markdown("# [Contributors](https://emtech-2-final-project-contributors.streamlit.app/)")
