65450100ca538c05f184480e1dcf0ed1c40db69f
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 22:22:22 +0800

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -11,3 +11,5 @@ background-size: cover;
 st.markdown(page_bg_img, unsafe_allow_html=True)
 
 st.title("About the App")
+st.divider()
+st.subheader("Introducing EcoSort, the innovative app designed to simplify waste management by classifying images of trash. Using advanced image recognition technology, EcoSort accurately identifies and categorizes items as cardboard, glass, metal, paper, plastic, or others. Simply snap a photo, and let EcoSort do the sorting for you, ensuring your recyclables end up in the right place. Perfect for both personal and community use, EcoSort promotes environmental responsibility with ease. Transform your waste disposal habits and contribute to a greener planet with EcoSort.")
