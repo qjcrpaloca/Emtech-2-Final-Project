3b452b4ad435caaf74179563d65b8e9990e54355
qjcrpaloca <146041155+qjcrpaloca@users.noreply.github.com>
2024-05-17 17:50:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,24 @@ def load_model():
   model=tf.keras.models.load_model('FineTunedTrashCNN.h5')
   return model
 model=load_model()
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://i.postimg.cc/4xgNnkfX/Untitled-design.png");
+background-size: cover;
+background-position: center center;
+background-repeat: no-repeat;
+background-attachment: local;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 st.title(':green[Trash Classifier App]')
 st.write("""
 # Garbage Classification\n
